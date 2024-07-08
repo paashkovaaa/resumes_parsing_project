c4bf1b65269d563c3e5aa5fdbbe45c9a3ff6899c
Yuliana <pashkova.yuliana@gmail.com>
2024-07-08 23:27:23 +0300

    split all criterias to different methods

--- parsers/robota_ua_parser.py
+++ parsers/robota_ua_parser.py
@@ -9,26 +9,37 @@ from bs4 import BeautifulSoup
 from data.resume import Resume
 
 
+class WebDriverConfig:
+    @staticmethod
+    def get_driver():
+        options = ChromeOptions()
+        options.headless = True  # Set to True for production
+        options.add_argument("--disable-blink-features=AutomationControlled")
+        options.add_argument("--incognito")
+
+        return webdriver.Chrome(
+            service=ChromeService(ChromeDriverManager().install()), options=options
+        )
+
+
 class RobotaUAParser:
     BASE_URL = "https://robota.ua/candidates"
 
     @staticmethod
     def fetch_resumes(position, location=None):
+        """
+        Fetches resumes from Robota.ua based on the given position and location.
+
+        Args:
+            position (str): The job position to search for.
+            location (str, optional): The location to search in. Defaults to None.
+
+        Returns:
+            list: A list of Resume objects.
+        """
         try:
-            options = ChromeOptions()
-            options.headless = False  # Set to True for production
-            options.add_argument("--disable-blink-features=AutomationControlled")
-            options.add_argument("--incognito")
-
-            driver = webdriver.Chrome(
-                service=ChromeService(ChromeDriverManager().install()), options=options
-            )
-
-            if location:
-                url = f"{RobotaUAParser.BASE_URL}/{position}/{location}/"
-            else:
-                url = f"{RobotaUAParser.BASE_URL}/{position}/ukraine/"
-
+            driver = WebDriverConfig.get_driver()
+            url = RobotaUAParser._build_url(position, location)
             driver.get(url)
 
             WebDriverWait(driver, 10).until(
@@ -39,32 +50,28 @@ class RobotaUAParser:
             soup = BeautifulSoup(page_content, "html.parser")
             resumes = []
 
-            resume_links = soup.find_all("a", class_="santa-no-underline")
+            resume_links = RobotaUAParser._extract_resume_links(soup)
             if not resume_links:
                 print("No resume links found.")
                 driver.quit()
                 return []
 
             for link in resume_links:
-                href = link.get("href")
-                if href and href.startswith("/candidates/"):
-                    resume_id = href.split("/")[-1]
-                    resume_url = f"{RobotaUAParser.BASE_URL}/{resume_id}/"
-
-                    driver.get(resume_url)
-                    WebDriverWait(driver, 10).until(
-                        EC.presence_of_element_located(
-                            (By.CLASS_NAME, "santa-typo-regular")
-                        )
+                resume_url = RobotaUAParser._build_resume_url(link)
+                driver.get(resume_url)
+                WebDriverWait(driver, 10).until(
+                    EC.presence_of_element_located(
+                        (By.CLASS_NAME, "santa-typo-regular")
                     )
+                )
 
-                    resume_page_content = driver.page_source
-                    resume_soup = BeautifulSoup(resume_page_content, "html.parser")
-                    resume = RobotaUAParser.parse_resume(resume_soup)
-                    if resume:
-                        resumes.append(resume)
-                    else:
-                        print(f"Failed to parse resume with ID: {resume_id}")
+                resume_page_content = driver.page_source
+                resume_soup = BeautifulSoup(resume_page_content, "html.parser")
+                resume = RobotaUAParser.parse_resume(resume_soup)
+                if resume:
+                    resumes.append(resume)
+                else:
+                    print(f"Failed to parse resume at URL: {resume_url}")
 
             driver.quit()
             return resumes
@@ -74,51 +81,97 @@ class RobotaUAParser:
             return []
 
     @staticmethod
+    def _build_url(position, location):
+        if location:
+            return f"{RobotaUAParser.BASE_URL}/{position}/{location}/"
+        else:
+            return f"{RobotaUAParser.BASE_URL}/{position}/ukraine/"
+
+    @staticmethod
+    def _extract_resume_links(soup):
+        return [
+            link.get("href")
+            for link in soup.find_all("a", class_="santa-no-underline")
+            if link.get("href") and link.get("href").startswith("/candidates/")
+        ]
+
+    @staticmethod
+    def _build_resume_url(link):
+        resume_id = link.split("/")[-1]
+        return f"{RobotaUAParser.BASE_URL}/{resume_id}/"
+
+    @staticmethod
     def parse_resume(soup):
+        """
+        Parses a resume from the given BeautifulSoup object.
+        """
         try:
-            position_tag = soup.find(
-                "p", class_="santa-mt-10 santa-typo-secondary santa-text-black-700"
-            )
-            job_position = (
-                position_tag.get_text(strip=True) if position_tag else "Unknown"
-            )
-
-            experience_tag = soup.find(
-                "span",
-                class_="santa-text-red-500 santa-whitespace-nowrap",
-            )
-            experience = (
-                experience_tag.get_text(strip=True) if experience_tag else "Unknown"
-            )
-
-            skills_tag = soup.find(
-                "div",
-                class_="santa-m-0 santa-mb-20 760:santa-mb-40 last:santa-mb-0 santa-typo-regular santa-text-black-700 santa-list empty:santa-hidden",
-            )
-            skills = skills_tag.get_text(strip=True) if skills_tag else "Unknown"
-
-            location_tag = soup.find("lib-resume-main-info").find(
-                "p", class_="santa-typo-regular santa-text-black-700"
-            )
-
-            location = location_tag.get_text(strip=True) if location_tag else "Unknown"
-
-            resume_main_info = soup.find("lib-resume-main-info")
-
-            if resume_main_info.find(
-                "p", class_="santa-flex santa-items-center santa-mb-10"
-            ):
-                salary_tag = resume_main_info.find(
-                    "p",
-                    class_="santa-flex santa-items-center santa-mb-10",
-                ).find("span", class_="santa-typo-regular santa-text-black-700")
-                salary = salary_tag.get_text(strip=True)
-
-            else:
-                salary = "Unknown"
+            job_position = RobotaUAParser._extract_job_position(soup)
+            experience = RobotaUAParser._extract_experience(soup)
+            skills = RobotaUAParser._extract_skills(soup)
+            location = RobotaUAParser._extract_location(soup)
+            salary = RobotaUAParser._extract_salary(soup)
 
             return Resume(job_position, experience, skills, location, salary)
 
         except Exception as e:
             print(f"Error parsing individual resume: {e}")
             return None
+
+    @staticmethod
+    def _extract_job_position(soup):
+        """
+        Extracts the job position from the resume.
+        """
+        position_tag = soup.find(
+            "p", class_="santa-mt-10 santa-typo-secondary santa-text-black-700"
+        )
+        return position_tag.get_text(strip=True) if position_tag else "Unknown"
+
+    @staticmethod
+    def _extract_experience(soup):
+        """
+        Extracts the work experience from the resume.
+        """
+        experience_tag = soup.find(
+            "span",
+            class_="santa-text-red-500 santa-whitespace-nowrap",
+        )
+        return experience_tag.get_text(strip=True) if experience_tag else "Unknown"
+
+    @staticmethod
+    def _extract_skills(soup):
+        """
+        Extracts the skills from the resume.
+        """
+        skills_tag = soup.find(
+            "div",
+            class_="santa-m-0 santa-mb-20 760:santa-mb-40 last:santa-mb-0 santa-typo-regular santa-text-black-700 santa-list empty:santa-hidden",
+        )
+        return skills_tag.get_text(strip=True) if skills_tag else "Unknown"
+
+    @staticmethod
+    def _extract_location(soup):
+        """
+        Extracts the location from the resume.
+        """
+        location_tag = soup.find("lib-resume-main-info").find(
+            "p", class_="santa-typo-regular santa-text-black-700"
+        )
+        return location_tag.get_text(strip=True) if location_tag else "Unknown"
+
+    @staticmethod
+    def _extract_salary(soup):
+        """
+        Extracts the salary from the resume.
+        """
+        resume_main_info_tag = soup.find("lib-resume-main-info")
+
+        if resume_main_info_tag.find(
+            "p", class_="santa-flex santa-items-center santa-mb-10"
+        ):
+            salary_tag = resume_main_info_tag.find(
+                "p",
+                class_="santa-flex santa-items-center santa-mb-10",
+            ).find("span", class_="santa-typo-regular santa-text-black-700")
+            return salary_tag.get_text(strip=True) if salary_tag else "Unknown"
--- parsers/work_ua_parser.py
+++ parsers/work_ua_parser.py
@@ -10,24 +10,25 @@ class WorkUAParser:
 
     @staticmethod
     def fetch_resumes(position, location=None):
-        try:
-            if location:
-                url = f"{WorkUAParser.BASE_URL}-{location}-{position}/"
-            else:
-                url = f"{WorkUAParser.BASE_URL}/-{position}/"
+        """
+        Fetches resumes from Work.ua based on the given position and location.
 
+        Args:
+            position (str): The job position to search for.
+            location (str, optional): The location to search in. Defaults to None.
+
+        Returns:
+            list: A list of Resume objects.
+        """
+        try:
+            url = WorkUAParser._build_url(position, location)
             response = requests.get(url)
             response.raise_for_status()
 
             soup = BeautifulSoup(response.content, "html.parser")
             resumes = []
 
-            resume_ids = [
-                a_tag["name"]
-                for a_tag in soup.select("a[name]")
-                if a_tag["name"].isdigit()
-            ]
-
+            resume_ids = WorkUAParser._extract_resume_ids(soup)
             for resume_id in resume_ids:
                 resume_url = f"{WorkUAParser.BASE_URL}/{resume_id}/"
                 resume_response = requests.get(resume_url)
@@ -49,71 +50,103 @@ class WorkUAParser:
             return []
 
     @staticmethod
+    def _build_url(position, location):
+        if location:
+            return f"{WorkUAParser.BASE_URL}-{location}-{position}/"
+        else:
+            return f"{WorkUAParser.BASE_URL}/-{position}/"
+
+    @staticmethod
+    def _extract_resume_ids(soup):
+        return [
+            a_tag["name"] for a_tag in soup.select("a[name]") if a_tag["name"].isdigit()
+        ]
+
+    @staticmethod
     def parse_resume(soup):
+        """
+        Parses a resume from the given BeautifulSoup object.
+        """
         try:
-            position_tag = soup.find("h2", class_="mt-lg sm:mt-xl")
-            job_position = (
-                position_tag.get_text(strip=True) if position_tag else "Unknown"
-            )
-
-            experience_field = soup.find("h2", text="Досвід роботи")
-            if not experience_field:
-                experience = "0 years, 0 months"
-            else:
-                experience_tags = []
-                for sibling in experience_field.find_next_siblings():
-                    if (
-                        sibling.name == "h2"
-                        and sibling.get_text(strip=True) == "Освіта"
-                    ):
-                        break
-                    experience_tags.extend(
-                        sibling.find_all("span", class_="text-default-7")
-                    )
-
-                total_experience_years = 0
-                total_experience_months = 0
-                for tag in experience_tags:
-                    experience_text = tag.get_text(strip=True)
-                    years_match = re.search(
-                        r"(\d+)\s*(рік|роки|років)", experience_text
-                    )
-                    months_match = re.search(
-                        r"(\d+)\s*(місяць|місяці|місяців)", experience_text
-                    )
-
-                    if years_match:
-                        total_experience_years += int(years_match.group(1))
-                    if months_match:
-                        total_experience_months += int(months_match.group(1))
-
-                total_experience_years += total_experience_months // 12
-                total_experience_months = total_experience_months % 12
-                experience = (
-                    f"{total_experience_years} years, {total_experience_months} months"
-                )
-
-            skills_tags = soup.find_all(
-                "span",
-                class_="label label-skill label-gray-100",
-            )
-            skills = [
-                tag.find("span", class_="ellipsis").get_text(strip=True)
-                for tag in skills_tags
-            ]
-
-            location_tag = soup.find("dt", text="Місто проживання:")
-            location = (
-                location_tag.find_next_sibling("dd").get_text(strip=True)
-                if location_tag
-                else "Unknown"
-            )
-
-            salary_tag = soup.find("span", class_="text-muted-print")
-            salary = salary_tag.get_text(strip=True) if salary_tag else "Unknown"
+            job_position = WorkUAParser._extract_job_position(soup)
+            experience = WorkUAParser._extract_experience(soup)
+            skills = WorkUAParser._extract_skills(soup)
+            location = WorkUAParser._extract_location(soup)
+            salary = WorkUAParser._extract_salary(soup)
 
             return Resume(job_position, experience, skills, location, salary)
 
         except Exception as e:
             print(f"Error parsing individual resume: {e}")
             return None
+
+    @staticmethod
+    def _extract_job_position(soup):
+        """
+        Extracts the job position from the resume.
+        """
+        position_tag = soup.find("h2", class_="mt-lg sm:mt-xl")
+        return position_tag.get_text(strip=True) if position_tag else "Unknown"
+
+    @staticmethod
+    def _extract_experience(soup):
+        """
+        Extracts the work experience from the resume.
+        """
+        experience_field = soup.find("h2", text="Досвід роботи")
+        if not experience_field:
+            return "0 years, 0 months"
+
+        experience_tags = []
+        for sibling in experience_field.find_next_siblings():
+            if sibling.name == "h2" and sibling.get_text(strip=True) == "Освіта":
+                break
+            experience_tags.extend(sibling.find_all("span", class_="text-default-7"))
+
+        total_experience_years, total_experience_months = 0, 0
+        for tag in experience_tags:
+            experience_text = tag.get_text(strip=True)
+            years_match = re.search(r"(\d+)\s*(рік|роки|років)", experience_text)
+            months_match = re.search(
+                r"(\d+)\s*(місяць|місяці|місяців)", experience_text
+            )
+
+            if years_match:
+                total_experience_years += int(years_match.group(1))
+            if months_match:
+                total_experience_months += int(months_match.group(1))
+
+        total_experience_years += total_experience_months // 12
+        total_experience_months = total_experience_months % 12
+        return f"{total_experience_years} years, {total_experience_months} months"
+
+    @staticmethod
+    def _extract_skills(soup):
+        """
+        Extracts the skills from the resume.
+        """
+        skills_tags = soup.find_all("span", class_="label label-skill label-gray-100")
+        return [
+            tag.find("span", class_="ellipsis").get_text(strip=True)
+            for tag in skills_tags
+        ]
+
+    @staticmethod
+    def _extract_location(soup):
+        """
+        Extracts the location from the resume.
+        """
+        location_tag = soup.find("dt", text="Місто проживання:")
+        return (
+            location_tag.find_next_sibling("dd").get_text(strip=True)
+            if location_tag
+            else "Unknown"
+        )
+
+    @staticmethod
+    def _extract_salary(soup):
+        """
+        Extracts the salary from the resume.
+        """
+        salary_tag = soup.find("span", class_="text-muted-print")
+        return salary_tag.get_text(strip=True) if salary_tag else "Unknown"
