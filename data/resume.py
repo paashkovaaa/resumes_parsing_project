b670b752628c623cd4b7bf6082c7230995afc621
Yuliana <pashkova.yuliana@gmail.com>
2024-07-09 16:49:49 +0300

    create dataclass instead of simple class & add score method to calculate score for each resume

--- data/resume.py
+++ data/resume.py
@@ -1,16 +1,54 @@
-class Resume:
-    def __init__(self, position, experience, skills, location, salary):
-        self.position = position
-        self.experience = experience
-        self.skills = skills
-        self.location = location
-        self.salary = salary
+from dataclasses import dataclass
+from typing import List
 
-    def __repr__(self):
-        return (
-            f"Position: {self.position}, "
-            f"experience: {self.experience},  "
-            f"skills: {self.skills}, "
-            f"location: {self.location}, "
-            f"salary expectation: {self.salary}, "
-        )
+
+@dataclass
+class Resume:
+    position: str
+    experience: str
+    skills: List[str]
+    location: str
+    salary: str
+    link: str = ""
+
+    def score(self, keywords: List[str]):
+        """
+        Calculate the relevance score of the resume based on various attributes.
+
+        Scoring Criteria:
+        - Job position: +2 points if specified.
+        - Experience: +2 points if specified.
+        - Skills: +2 points if specified.
+        - Location: +1 point if specified.
+        - Salary: +1 point if specified.
+        - Skills matching keywords: +2 points for each matching skill.
+
+        Args:
+            keywords (List[str]): List of keywords to match in the skills section.
+
+        Returns:
+            int: Total relevance score for the resume.
+        """
+
+        score = 0
+
+        if self.position != "Unknown":
+            score += 2
+
+        if self.experience != "Unknown":
+            score += 2
+
+        if self.skills != "Unknown":
+            score += 2
+
+        if self.location != "Unknown":
+            score += 1
+
+        if self.salary != "Unknown":
+            score += 1
+
+        skill_keywords = set(keywords)
+        matching_skills = skill_keywords.intersection(set(self.skills))
+        score += 2 * len(matching_skills)
+
+        return score
