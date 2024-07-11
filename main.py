ac9e8d4dfafde229f04ed7ef422e49c91aac8ff4
Yuliana <pashkova.yuliana@gmail.com>
2024-07-11 17:36:29 +0300

    add logic to start app

--- main.py
+++ main.py
@@ -1,28 +1,58 @@
-from parsers.robota_ua_parser import RobotaUAParser
-from parsers.work_ua_parser import WorkUAParser
-from utils.filters import sort_resumes_by_relevance
+import os
+
+from telegram.ext import (
+    ConversationHandler,
+    CommandHandler,
+    MessageHandler,
+    filters,
+    Application,
+)
+
+from telegram_bot.telegram_bot import (
+    SELECT_SITE,
+    select_site,
+    SET_POSITION,
+    set_position,
+    SET_LOCATION,
+    SET_KEYWORDS,
+    FETCH_RESUMES,
+    set_location,
+    set_keywords,
+    fetch_resumes,
+    cancel,
+)
+
+from telegram_bot.telegram_bot import start
 
 
-def main():
-    position = "data-scientist"
-    location = "kyiv"
-    keywords = ["Python", "Machine Learning", "Data Analysis", "SQL"]
+def main() -> None:
+    application = (
+        Application.builder().token(os.environ.get("TELEGRAM_BOT_TOKEN")).build()
+    )
 
-    try:
-        all_resumes = RobotaUAParser.fetch_resumes(
-            position, location
-        ) + WorkUAParser.fetch_resumes(position, location)
+    conv_handler = ConversationHandler(
+        entry_points=[CommandHandler("start", start)],
+        states={
+            SELECT_SITE: [MessageHandler(filters.TEXT & ~filters.COMMAND, select_site)],
+            SET_POSITION: [
+                MessageHandler(filters.TEXT & ~filters.COMMAND, set_position)
+            ],
+            SET_LOCATION: [
+                MessageHandler(filters.TEXT & ~filters.COMMAND, set_location)
+            ],
+            SET_KEYWORDS: [
+                MessageHandler(filters.TEXT & ~filters.COMMAND, set_keywords)
+            ],
+            FETCH_RESUMES: [CommandHandler("fetch", fetch_resumes)],
+        },
+        fallbacks=[CommandHandler("cancel", cancel)],
+    )
 
-        if not all_resumes:
-            print("No resumes fetched or an error occurred.")
-        else:
-            sorted_resumes = sort_resumes_by_relevance(all_resumes, keywords)
-            for resume in sorted_resumes:
-                print(f"Resume URL: {resume.link}, Score: {resume.score(keywords)}")
-                print(resume)
+    application.add_handler(conv_handler)
 
-    except Exception as e:
-        print(f"Error in main function: {e}")
+    application.add_handler(CommandHandler("start", start))
+
+    application.run_polling()
 
 
 if __name__ == "__main__":
